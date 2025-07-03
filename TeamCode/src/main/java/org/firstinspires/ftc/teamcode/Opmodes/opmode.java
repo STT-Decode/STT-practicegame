2630b7e78a1715030f681bc704c82e6e3dd9a8ef
FTC computer links <ftcscw@gmail.com>
2025-07-03 14:23:46 +0200

    OPmodes + aalparts

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/opmode.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.Opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.RobotParts.All_Parts;
+
+
+//the name is how this Opmode will show up on the driver-hub
+@TeleOp(name = "Opmode", group = "TeleOp")
+public class opmode extends LinearOpMode
+{
+    private ElapsedTime runtime = new ElapsedTime();
+    All_Parts allParts = new All_Parts();
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        allParts.init(hardwareMap);
+        double clawPos = 0;
+        double rotationPos = 0;
+        boolean canToggleClaw = true;
+        boolean canToggleRotation = true;
+
+        waitForStart();
+        if (isStopRequested()) return;
+        while (opModeIsActive())
+        {
+            double y = -gamepad1.left_stick_x;
+            double x = gamepad1.left_stick_y;
+            double rotate = 0.6 * gamepad1.right_stick_x;
+            double speed = gamepad1.right_trigger;
+            double armPower = gamepad2.right_stick_y;
+
+            if (canToggleClaw && gamepad2.left_bumper)
+            {
+                clawPos = Math.abs(clawPos - 1);
+                canToggleClaw = false;
+            } else if (!gamepad2.left_bumper)
+            {
+                canToggleClaw = true;
+            }
+
+            if (canToggleRotation && gamepad2.right_bumper)
+            {
+                clawPos = Math.abs(rotationPos - 1);
+                canToggleRotation = false;
+            } else if (!gamepad2.right_bumper)
+            {
+                canToggleRotation = true;
+            }
+
+            allParts.drive0(y, x, rotate, speed);
+            allParts.setArmPower(armPower);
+            allParts.setClawPos(clawPos);
+        }
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/All_Parts.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode.RobotParts;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class All_Parts
+{
+    private DcMotorEx lf;
+    private DcMotorEx rf;
+    private DcMotorEx lb;
+    private DcMotorEx rb;
+    private DcMotorEx arm;
+    private Servo claw;
+    private Servo rotationServo;
+    private double maxPower;
+
+
+    public void init(HardwareMap map)
+    {
+
+        lf = map.get(DcMotorEx.class, "left_front");
+        rf = map.get(DcMotorEx.class, "right_front");
+        lb = map.get(DcMotorEx.class, "left_back");
+        rb = map.get(DcMotorEx.class, "right_back");
+        arm = map.get(DcMotorEx.class, "arm");
+        claw = map.get(Servo.class, "claw");
+        rotationServo = map.get(Servo.class,"rotationServo");
+
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+
+
+    public void drive0(double forward, double right, double rotate, double power)
+    {
+        double leftFrontPower = -forward - right + rotate;
+        double rightFrontPower = -forward + right - rotate;
+        double rightRearPower = -forward - right - rotate;
+        double leftRearPower = -forward + right + rotate;
+
+        maxPower = power;
+        maxPower = Math.max(maxPower, Math.abs(leftFrontPower));
+        maxPower = Math.max(maxPower, Math.abs(rightFrontPower));
+        maxPower = Math.max(maxPower, Math.abs(rightRearPower));
+        maxPower = Math.max(maxPower, Math.abs(leftRearPower));
+
+        lf.setPower(leftFrontPower / maxPower);
+        rf.setPower(-rightFrontPower / maxPower);
+        rb.setPower(-rightRearPower / maxPower);
+        lb.setPower(leftRearPower / maxPower);
+    }
+
+    public void drive1(float fl, float fr, float br, float bl, double weight)
+    {
+        lf.setPower(fl / weight);
+        rf.setPower(fr / weight);
+        rb.setPower(br / weight);
+        lb.setPower(bl / weight);
+    }
+
+    public void setArmPower(double power)
+    {
+        arm.setPower(power);
+    }
+
+    public void setClawPos(double pos)
+    {
+        claw.setPosition(pos);
+    }
+
+    public void setRotationPos(double pos)
+    {
+        rotationServo.setPosition(pos);
+    }
+
+}
